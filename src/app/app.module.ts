3a77f264d8142f67ed0f094663deb7f536667290
ImsirovicAjdin <ajdika@live.com>
2019-09-02 10:52:54 +0200

    Adding the sortable component, part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { RatingModule } from 'ngx-bootstrap/rating';
+import { SortableModule } from 'ngx-bootstrap/sortable';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { DemoRatingSelectOnEnterComponent } from './rating/rating.component';
+import { ComplexDatamodelDemoComponent } from './sortable/sortable.component';
 
 @NgModule({
   imports: [ 
     BrowserModule, 
     FormsModule,
-    RatingModule.forRoot() 
+    RatingModule.forRoot(),
+    SortableModule.forRoot() 
   ],
   declarations: [ 
     AppComponent, 
     HelloComponent,
-    DemoRatingSelectOnEnterComponent
+    DemoRatingSelectOnEnterComponent, ComplexDatamodelDemoComponent
   ],
   bootstrap:    [ AppComponent ]
 })
