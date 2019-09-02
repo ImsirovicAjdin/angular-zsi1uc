6a7984e20cc423e69c966d1a0107ddb0cb9c7fe6
ImsirovicAjdin <ajdika@live.com>
2019-09-02 11:38:21 +0200

    Adding the tooltip ngx-bootstrap component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,14 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { RatingModule } from 'ngx-bootstrap/rating';
 import { SortableModule } from 'ngx-bootstrap/sortable';
 import { TimepickerModule } from 'ngx-bootstrap/timepicker';
-
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { DemoRatingSelectOnEnterComponent } from './rating/rating.component';
 import { ComplexDatamodelDemoComponent } from './sortable/sortable.component';
 import { DemoTimepickerFormComponent } from './timepicker/timepicker.component';
+import { DemoTooltipDynamicComponent } from './tooltip/tooltip.component';
 
 @NgModule({
   imports: [ 
@@ -19,14 +20,16 @@ import { DemoTimepickerFormComponent } from './timepicker/timepicker.component';
     FormsModule, ReactiveFormsModule,
     RatingModule.forRoot(),
     SortableModule.forRoot(),
-    TimepickerModule.forRoot()
+    TimepickerModule.forRoot(),
+    TooltipModule.forRoot()
   ],
   declarations: [ 
     AppComponent, 
     HelloComponent,
     DemoRatingSelectOnEnterComponent, 
     ComplexDatamodelDemoComponent,
-    DemoTimepickerFormComponent
+    DemoTimepickerFormComponent,
+    DemoTooltipDynamicComponent
   ],
   bootstrap:    [ AppComponent ]
 })
