7005b2a937d64c4d545f7581127c04e9d5cdb842
ImsirovicAjdin <ajdika@live.com>
2019-09-02 11:17:23 +0200

    Adding the timepicker component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RatingModule } from 'ngx-bootstrap/rating';
 import { SortableModule } from 'ngx-bootstrap/sortable';
+import { TimepickerModule } from 'ngx-bootstrap/timepicker';
+
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { DemoRatingSelectOnEnterComponent } from './rating/rating.component';
 import { ComplexDatamodelDemoComponent } from './sortable/sortable.component';
+import { DemoTimepickerFormComponent } from './timepicker/timepicker.component';
 
 @NgModule({
   imports: [ 
     BrowserModule, 
-    FormsModule,
+    FormsModule, ReactiveFormsModule,
     RatingModule.forRoot(),
-    SortableModule.forRoot() 
+    SortableModule.forRoot(),
+    TimepickerModule.forRoot()
   ],
   declarations: [ 
     AppComponent, 
     HelloComponent,
-    DemoRatingSelectOnEnterComponent, ComplexDatamodelDemoComponent
+    DemoRatingSelectOnEnterComponent, 
+    ComplexDatamodelDemoComponent,
+    DemoTimepickerFormComponent
   ],
   bootstrap:    [ AppComponent ]
 })
